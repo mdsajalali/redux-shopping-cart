a4938a99befddcfbf79ef438039551ae06f74628
mdsajalali <itzsajal.me@gmail.com>
2022-10-31 21:39:50 +0600

    first commit

---
+++ src/components/CardsData.js
@@ -0,0 +1,139 @@
+const Cardsdata = [
+  {
+    id: 350,
+    rname: "Cakes",
+    imgdata:
+      "https://static.hungrynaki.com/hungrynaki-v4/restaurants/rohan_s_hot_cake/meta/rohan_s_hot_cake_cover_1636005001316.jpeg",
+    address: "Masterchef Rajshahi",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: " 1175 + order placed from here recently",
+    price: 350,
+    rating: "3.8",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 2,
+    rname: "Burger",
+    imgdata:
+      "https://static.hungrynaki.com/hungrynaki-v4/restaurants/burg_bar/meta/burg_bar_cover_1645076628447.jpeg",
+    address: "Street Food",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: " 2525 + order placed from here recently",
+    price: 100,
+    rating: "3.9",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 3,
+    rname: "Kebab",
+    imgdata:
+      "https://static.hungrynaki.com/hungrynaki-v4/restaurants/barbeque_station/meta/barbeque_station_cover_1638895487364.jpeg",
+    address: "Pizza, Fast Food, Pasta",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: " 650 + order placed from here recently",
+    price: 470,
+    rating: "4.2",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 4,
+    rname: "Thai",
+    imgdata:
+      "https://static.hungrynaki.com/hungrynaki-v4/restaurants/meta/_cover_1611461993781.jpg",
+    address: "Momos",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: " 300 + order placed from here recently",
+    price: 760,
+    rating: "3.8",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 5,
+    rname: "Biriyani",
+    imgdata:
+      "https://static.hungrynaki.com/hungrynaki-v4/restaurants/grand_kashmiri_biriyani_house/meta/grand_kashmiri_biriyani_house_cover_1653905303867.jpeg",
+    address: "Masterchef Rajshahi",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: "1050 + order placed from here recently",
+    price: 210,
+    rating: "4.0",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 6,
+    rname: "Pizza",
+    imgdata:
+      "https://static.hungrynaki.com/hungrynaki-v4/restaurants/pizza_burg/items/pizza_burg_meaty_onion_pizza_thumbnail_1613476625464.jpg",
+    address: "Wraps FastFood, Chines",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: " 1100 + order placed from here recently",
+    price: 200,
+    rating: "3.8",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 7,
+    rname: "Pasta",
+    imgdata:
+      "https://static.hungrynaki.com/hungrynaki-v4/restaurants/m_e_m_e_c_a_f_e/meta/m_e_m_e_c_a_f_e_cover_1657016612620.jpeg",
+    address: "Masterchef Rajshahi",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: "500 + order placed from here recently",
+    price: 1000,
+    rating: "3.8",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 8,
+    rname: "Pizza",
+    imgdata:
+      "https://static.hungrynaki.com/hungrynaki-v4/restaurants/the_eatalia/meta/the_eatalia_cover_1635093844666.jpeg",
+    address: "Masterchef Rajshahi",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: "500 + order placed from here recently",
+    price: 700,
+    rating: "3.2",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 9,
+    rname: "French Fry",
+    imgdata:
+      "https://static.hungrynaki.com/hungrynaki-v4/restaurants/burg_bar/items/burg_bar_french_fry_thumbnail_1645105007432.jpeg",
+    address: "Masterchef Rajshahi",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: "2525 + order placed from here recently",
+    price: 250,
+    rating: "3.8",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+];
+
+export default Cardsdata;
